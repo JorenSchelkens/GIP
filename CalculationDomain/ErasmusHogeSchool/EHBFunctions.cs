7003c190a6a1eabdad96186113fb1752a14ec594
Joren Schelkens <52953553+JorenSchelkens@users.noreply.github.com>
2020-09-21 17:09:42 +0200

    Bugfix

--- CalculationDomain/ErasmusHogeSchool/EHBFunctions.cs
+++ CalculationDomain/ErasmusHogeSchool/EHBFunctions.cs
@@ -50,23 +50,23 @@ namespace CalculationDomain.ErasmusHogeSchool
                 current = current.AddYears(-1);
             }
 
-            result.Add($"<b>Doorstroom</b> {(current.Year - 6).ToString().Substring(2)}-{(current.Year - 5).ToString().Substring(2)} <br>");
-            result.Add($"<b>Doorstroom</b>  {(current.Year - 5).ToString().Substring(2)}-{(current.Year - 4).ToString().Substring(2)} <br>");
+            result.Add($"<b>Doorstroom</b> {(current.Year - 5).ToString().Substring(2)}-{(current.Year - 4).ToString().Substring(2)} <br>");
             result.Add($"<b>Doorstroom</b>  {(current.Year - 4).ToString().Substring(2)}-{(current.Year - 3).ToString().Substring(2)} <br>");
             result.Add($"<b>Doorstroom</b>  {(current.Year - 3).ToString().Substring(2)}-{(current.Year - 2).ToString().Substring(2)} <br>");
             result.Add($"<b>Doorstroom</b>  {(current.Year - 2).ToString().Substring(2)}-{(current.Year - 1).ToString().Substring(2)} <br>");
+            result.Add($"<b>Doorstroom</b>  {(current.Year - 1).ToString().Substring(2)}-{(current.Year).ToString().Substring(2)} <br>");
 
-            result.Add($"<b>Instroom</b> {(current.Year - 5).ToString().Substring(2)}-{(current.Year - 4).ToString().Substring(2)} <br>");
             result.Add($"<b>Instroom</b> {(current.Year - 4).ToString().Substring(2)}-{(current.Year - 3).ToString().Substring(2)} <br>");
             result.Add($"<b>Instroom</b> {(current.Year - 3).ToString().Substring(2)}-{(current.Year - 2).ToString().Substring(2)} <br>");
             result.Add($"<b>Instroom</b> {(current.Year - 2).ToString().Substring(2)}-{(current.Year - 1).ToString().Substring(2)} <br>");
             result.Add($"<b>Instroom</b> {(current.Year - 1).ToString().Substring(2)}-{(current.Year).ToString().Substring(2)} <br>");
+            result.Add($"<b>Instroom</b> {(current.Year).ToString().Substring(2)}-{(current.Year + 1).ToString().Substring(2)} <br>");
 
-            result.Add($"<b>Uitstroom</b> {(current.Year - 6).ToString().Substring(2)}-{(current.Year - 5).ToString().Substring(2)} <br>");
             result.Add($"<b>Uitstroom</b> {(current.Year - 5).ToString().Substring(2)}-{(current.Year - 4).ToString().Substring(2)} <br>");
             result.Add($"<b>Uitstroom</b> {(current.Year - 4).ToString().Substring(2)}-{(current.Year - 3).ToString().Substring(2)} <br>");
             result.Add($"<b>Uitstroom</b> {(current.Year - 3).ToString().Substring(2)}-{(current.Year - 2).ToString().Substring(2)} <br>");
             result.Add($"<b>Uitstroom</b> {(current.Year - 2).ToString().Substring(2)}-{(current.Year - 1).ToString().Substring(2)} <br>");
+            result.Add($"<b>Uitstroom</b> {(current.Year - 1).ToString().Substring(2)}-{(current.Year).ToString().Substring(2)} <br>");
 
             result.Add($"<b>EmptyPowerPoint</b>");
 
